196b525fe9b246ea5598491d22df70917fb6b02e
mb3112 <martin.budka@gmail.com>
2020-08-04 10:09:33 +0200

    r

--- kopirovanieKWpdf/Program.cs
+++ kopirovanieKWpdf/Program.cs
@@ -8,8 +8,12 @@ namespace kopirovanieKWpdf
         static void Main(string[] args)
         {
 
-            string endfolder = @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Woche";
-
+            //string endfolder = @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Woche";
+            //string endfolder = @"E:\prac\exportM";
+            string endfolder = @"E:\prac\exportM";
+            string[] cesta = { @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Woche", @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Monat" };
+            //string[] cesta = { @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Woche", @"Z:\Kvalita\Vyhodnotenia\Wochenubersicht pro Spedition\pro Monat" };
+           // Console.WriteLine(cesta[1]);
             string[] array1 = Directory.GetFiles(endfolder, "*.pdf");
             //Console.WriteLine("--- Files: ---");
 
@@ -22,20 +26,29 @@ namespace kopirovanieKWpdf
                 if (!Directory.Exists(pathString))
                 {
                     //Console.WriteLine(pathString);
-                    //Console.WriteLine(Path.GetFileName(name));
-                    //Console.WriteLine(Path.Combine(pathString, Path.GetFileName(name)));
-                    Directory.CreateDirectory(pathString);
-                    File.Move(name, Path.Combine(pathString, Path.GetFileName(name)), true);
+                    Console.WriteLine(Path.GetFileName(name));
+                    Console.WriteLine(Path.Combine(pathString, Path.GetFileName(name)));
+                    ////Directory.CreateDirectory(pathString);
+                    ////File.Move(name, Path.Combine(pathString, Path.GetFileName(name)), true);
                     //File.WriteAllText(@"C:\martin\prac\export\WriteText.txt", pathString);
-                    Console.WriteLine("--- :HOTOVO!: ---");
+                    cwtext();
                 }
                 else
                 {
-                    File.Move(name, Path.Combine(pathString, Path.GetFileName(name)));
+                    //File.Move(name, Path.Combine(pathString, Path.GetFileName(name)), true);
                 }
             }
 
+            cwtext();
+        }
+
+        static void cwtext()
+        {
             Console.WriteLine("--- :HOTOVO!: ---");
         }
+   
+        
+        
+      
     }
 }
